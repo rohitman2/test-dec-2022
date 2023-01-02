48c4252640474be0e7baf7c4cc9676fbf15cae71
Rohit Manshani <101917071+rohitman2@users.noreply.github.com>
2023-01-02 21:00:00 +0530

    Add files via upload

---
+++ HelloWorld/src/main/java/com/springboot/app/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Test
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
+  
