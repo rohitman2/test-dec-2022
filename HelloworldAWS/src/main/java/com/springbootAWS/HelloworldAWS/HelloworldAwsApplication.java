911bdc216a08dad19c59761e85f26c93cdbd05ab
Rohit Manshani <101917071+rohitman2@users.noreply.github.com>
2023-01-04 20:58:45 +0530

    Add files via upload

---
+++ HelloworldAWS/src/main/java/com/springbootAWS/HelloworldAWS/HelloworldAwsApplication.java
@@ -0,0 +1,15 @@
+package com.springbootAWS.HelloworldAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldAwsApplication.class, args);
+	}
+
+}
---
+++ HelloworldAWS/src/test/java/com/springbootAWS/HelloworldAWS/HelloworldAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootAWS.HelloworldAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
